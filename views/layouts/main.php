988d04b7e48f0c21f9a68869d8c8654a95bd7ddf
Pozitronik <antikillerxxl@gmail.com>
2019-05-03 18:23:28 +0300

    Main layout modified

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,69 +14,54 @@ use app\assets\AppAsset;
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
-<head>
-	<meta charset="<?= Yii::$app->charset ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php $this->registerCsrfMetaTags() ?>
-	<title><?= Html::encode($this->title) ?></title>
-	<?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
+	<!DOCTYPE html>
+	<html lang="<?= Yii::$app->language ?>">
+	<head>
+		<meta charset="<?= Yii::$app->charset ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php $this->registerCsrfMetaTags() ?>
+		<title><?= Html::encode($this->title) ?></title>
+		<?php $this->head() ?>
+	</head>
+	<body>
+	<?php $this->beginBody() ?>
 
-<div class="wrap">
-	<?php
-	NavBar::begin([
-		'brandLabel' => Yii::$app->name,
-		'brandUrl' => Yii::$app->homeUrl,
-		'options' => [
-			'class' => 'navbar-inverse navbar-fixed-top',
-		],
-	]);
-	echo Nav::widget([
-		'options' => ['class' => 'navbar-nav navbar-right'],
-		'items' => [
-			['label' => 'Home', 'url' => ['/site/index']],
-			['label' => 'About', 'url' => ['/site/about']],
-			['label' => 'Contact', 'url' => ['/site/contact']],
-			Yii::$app->user->isGuest?(
-			['label' => 'Login', 'url' => ['/site/login']]
-			):(
-				'<li>'
-				.Html::beginForm(['/site/logout'], 'post')
-				.Html::submitButton(
-					'Logout ('.Yii::$app->user->identity->username.')',
-					['class' => 'btn btn-link logout']
-				)
-				.Html::endForm()
-				.'</li>'
-			)
-		],
-	]);
-	NavBar::end();
-	?>
+	<div class="wrap">
+		<?php NavBar::begin([
+			'brandLabel' => 'ActiveRecord Logger test app',
+			'brandUrl' => '/data/index',
+			'options' => [
+				'class' => 'navbar-inverse navbar-fixed-top',
+			],
+		]); ?>
+		<?= Nav::widget([
+			'options' => ['class' => 'navbar-nav navbar-right'],
+			'items' => [
+				['label' => 'Data items', 'url' => ['/data/index']],
+				['label' => 'Item types', 'url' => ['/data-types/index']],
+				['label' => 'Item kinds', 'url' => ['/data-kinds/index']],
+			],
+		]); ?>
+		<?php NavBar::end(); ?>
 
-	<div class="container">
-		<?= Breadcrumbs::widget([
-			'links' => isset($this->params['breadcrumbs'])?$this->params['breadcrumbs']:[],
-		]) ?>
-		<?= Alert::widget() ?>
-		<?= $content ?>
+		<div class="container">
+			<?= Breadcrumbs::widget([
+				'links' => isset($this->params['breadcrumbs'])?$this->params['breadcrumbs']:[],
+			]) ?>
+			<?= Alert::widget() ?>
+			<?= $content ?>
+		</div>
 	</div>
-</div>
 
-<footer class="footer">
-	<div class="container">
-		<p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+	<footer class="footer">
+		<div class="container">
+			<p class="pull-left">This is test application for
+				<a href="https://github.com/pozitronik/arlogger">Yii2 ActiveRecord Logger</a></p>
+		</div>
+	</footer>
 
-		<p class="pull-right"><?= Yii::powered() ?></p>
-	</div>
-</footer>
-
-<?php $this->endBody() ?>
-</body>
-</html>
+	<?php $this->endBody() ?>
+	</body>
+	</html>
 <?php $this->endPage() ?>
