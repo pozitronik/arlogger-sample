ee8614b358aa9bc73e4f208667eb2d2d7126c0f4
Pozitronik <antikillerxxl@gmail.com>
2019-05-03 17:52:24 +0300

    Added relation properties and getters

--- models/Data.php
+++ models/Data.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\db\ActiveQuery;
 
 /**
  * This is the model class for table "data".
@@ -12,6 +13,10 @@ use Yii;
  * @property string $create_time Creation_time
  * @property int $type_id Type ID key
  * @property int $deleted Deleted item flag
+ *
+ * @property DataTypes|ActiveQuery $dataType Related DataType record
+ * @property RelDataKinds[]|ActiveQuery $relDataKinds Relation to binding table between DataKinds records
+ * @property DataKinds[]|ActiveQuery $dataKinds Related DataKinds records
  */
 class Data extends \yii\db\ActiveRecord {
 	/**
@@ -44,5 +49,26 @@ class Data extends \yii\db\ActiveRecord {
 			'type_id' => 'Type ID key',
 			'deleted' => 'Deleted item flag',
 		];
+	}
+
+	/**
+	 * @return DataTypes|ActiveQuery
+	 */
+	public function getDataType() {
+		return $this->hasOne(DataTypes::class, ['id' => 'type_id']);
+	}
+
+	/**
+	 * @return RelDataKinds[]|ActiveQuery
+	 */
+	public function getRelDataKinds() {
+		return $this->hasMany(RelDataKinds::class, ['data_id' => 'id']);
+	}
+
+	/**
+	 * @return DataKinds[]|ActiveQuery
+	 */
+	public function getDataKinds() {
+		return $this->hasMany(DataKinds::class, ['id' => 'kind_id'])->via('relDataKinds');
 	}
 }
